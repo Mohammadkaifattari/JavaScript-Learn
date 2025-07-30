1b8050bba46cb24b0bf6f53ba1faa26cb624897c
Kaif <kaifattari0123@gmail.com>
2025-07-30 14:52:28 +0500

    Class-3

--- js class3/index.js
+++ js class3/index.js
@@ -1,30 +1,77 @@
-//CALCULATOR
-var fristNumber =+prompt("Enter your frist Number Or Obtained Number")
-var SecNumber =+prompt("Enter your sec Number Or Total Number")
-var opreator = prompt("Enter Your Operator (+ , - , / , * , % )")
+// //CALCULATOR
+// var fristNumber =+prompt("Enter your frist Number Or Obtained Number")
+// var SecNumber =+prompt("Enter your sec Number Or Total Number")
+// var opreator = prompt("Enter Your Operator (+ , - , / , * , % )")
 
 
 
-if (opreator=="+") {
-    console.log( fristNumber+SecNumber)
-    
-}
-else if (opreator=="-") {
-    console.log(fristNumber-SecNumber) 
-}
-else if (opreator=="*") {
-    console.log(fristNumber*SecNumber)
-    
-}
-else if (opreator=="/") {
-    console.log(fristNumber/SecNumber)
-    
-}
-else if (opreator=="%") {
-    console.log(fristNumber/SecNumber*100+"%")
+// if (opreator=="+") {
+//     console.log( fristNumber+SecNumber)
 
+// }
+// else if (opreator=="-") {
+//     console.log(fristNumber-SecNumber) 
+// }
+// else if (opreator=="*") {
+//     console.log(fristNumber*SecNumber)
+
+// }
+// else if (opreator=="/") {
+//     console.log(fristNumber/SecNumber)
+
+// }
+// else if (opreator=="%") {
+//     console.log(fristNumber/SecNumber*100+"%")
+
+
+// }
+
+//Paractice
+
+//Prompt is a box that ask user for fill the box with info
+
+//= Assginment Operator zyad bakkar ke barraber he 
+//==Value checker  5=5 sahi kya 5>4 sahi
+
+// var Fristname = prompt("Frist Name")
+// var Secname = prompt("Secound Name")
+// alert("Wellcome "+Fristname+" " + Secname)
+ 
+//If Statment
+// var fnum=+prompt("Your Frist Number")
+// var snum=+prompt("Your 2nd Number")
+// var CalculatorSigns=prompt("Enter Your Sign(+,-,*,/)")
+
+// if (CalculatorSigns=="+") {console.log(fnum+snum)
+    
+// }else if (CalculatorSigns=="-") {console.log(fnum-snum)}
+// else if (CalculatorSigns=="*") {console.log(fnum*snum)
+    
+// }
+// else if (CalculatorSigns=="/") {console.log(fnum/snum)
+    
+// }
+
+// //Camparetive operater 
+// var nab=5>2 //Greater Than
+// var nab2=3<5 //Less Than
+// var nab3=5>=2 //Greater than or equal
+// var nab4=3<=5 //Less than or Eqaul
+// console.log(nab,nab2,nab3,nab4)
+
+
+var age = prompt("AGE ?")
+var Cinic=prompt ("DoYou Have CNIC ?")
+var Pak=prompt("Are You Pakistani  ?")
+
+
+if (age>=18 && Cinic=="yes" ||Cinic=="Yes" && Pak== "yes" || Pak=="Yes") 
+    {alert("CONGO....")
     
 }
+else (
+    alert("GHAR JAO")
+)
 
 
 
